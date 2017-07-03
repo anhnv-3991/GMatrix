b81437210de6686a3cdc79ea72b6cfcfedd7f121
anhnv-3991 <anhnv91@yahoo.com>
2017-07-03 11:28:18 +0900

    Added Vector, SquareMatrix, IdentityMatrix

--- GEigen/GEigen.h
+++ GEigen/GEigen.h
@@ -7,278 +7,13 @@
 
 namespace GEigen {
 
-class DiscreteMatrix {
-public:
-	CUDAH DiscreteMatrix() {
-		ul_offset_ = ur_offset_ = ll_offset_ = lr_offset_ = 0;
-		rows0_ = rows1_ = cols0_ = cols1_ = 0;
-		upper_left_ = upper_right_ = lower_left_ = lower_right_ = NULL;
-	}
-
-	CUDAH DiscreteMatrix(int ul_offset, int ur_offset, int ll_offset, int lr_offset,
-							int rows0, int rows1, int cols0, int cols1,
-							float *upper_left, float *upper_right, float *lower_left, float *lower_right) {
-		ul_offset_ = ul_offset;
-		ur_offset_ = ur_offset;
-		ll_offset_ = ll_offset;
-		lr_offset_ = lr_offset;
-
-		rows0_ = rows0;
-		rows1_ = rows1;
-		cols0_ = cols0;
-		cols1_ = cols1;
-
-		upper_left_ = upper_left;
-		upper_right_ = upper_right;
-		lower_left_ = lower_left;
-		lower_right_ = lower_right;
-	}
-
-	CUDAH float at(int row, int col) {
-		//Cell at upper left
-		if (row < rows0_ && row >= 0 && col < cols0_ && col >= 0) {
-			return upper_left_[(row * cols0_ + col) * ul_offset_];
-		}
-
-		//Cell at lower left
-		if (row >= rows0_ && row < (rows0_ + rows1_) && col < cols0_ && col >= 0) {
-			return lower_left_[((row - rows0_) * cols0_ + col) * ll_offset_];
-		}
-
-		//Cell at upper right
-		if (row < rows0_ && row >= 0 && col >= cols0_ && col < (cols0_ + cols1_)) {
-			return upper_right_[(row * cols0_ + col - cols0_) * ur_offset_];
-		}
-
-		//Cell at lower right
-		if (row >= rows0_ && row < (rows0_ + rows1_) && col >= cols0_ && col < (cols0_ + cols1_)) {
-			return lower_right_[((row - rows0_) * cols1_ + col - cols0_) * lr_offset_];
-		}
-
-		return -1;
-	}
-
-	CUDAH bool set(int row, int col, float val) {
-		//Cell at upper left
-		if (row < rows0_ && row >= 0 && col < cols0_ && col >= 0) {
-			upper_left_[(row * cols0_ + col) * ul_offset_] = val;
-			return true;
-		}
-
-		//Cell at lower left
-		if (row >= rows0_ && row < (rows0_ + rows1_) && col < cols0_ && col >= 0) {
-			lower_left_[((row - rows0_) * cols0_ + col) * ll_offset_] = val;
-			return true;
-		}
-
-		//Cell at upper right
-		if (row < rows0_ && row >= 0 && col >= cols0_ && col < (cols0_ + cols1_)) {
-			upper_right_[(row * cols0_ + col - cols0_) * ur_offset_] = val;
-			return true;
-		}
-
-		//Cell at lower right
-		if (row >= rows0_ && row < (rows0_ + rows1_) && col >= cols0_ && col < (cols0_ + cols1_)) {
-			lower_right_[((row - rows0_) * cols1_ + col - cols0_) * lr_offset_] = val;
-			return true;
-		}
-
-		return false;
-	}
-
-	CUDAH DiscreteMatrix getCol(int col) {
-		//Col at left half
-		if (col < cols0_ && col >= 0) {
-			return DiscreteMatrix(ul_offset_ * cols0_, 0, ll_offset_ * cols0_, 0, 
-									rows0_, rows1_, 1, 0, 
-									upper_left_ + col * ul_offset_, NULL, lower_left_ + col * ll_offset_, NULL);
-		}
-
-
-		//Col at right half
-		if (col >= cols0_ && col < (cols0_ + cols1_)) {
-			return DiscreteMatrix(ur_offset_ * cols1_, 0, lr_offset_ * cols1_, 0,
-									rows0_, rows1_, 1, 0,
-									upper_right_ + col * ur_offset_, NULL, lower_right_ + col * lr_offset_, NULL);
-		}
-
-		return DiscreteMatrix();
-	}
-
-	CUDAH DiscreteMatrix getRow(int row) {
-		//Row at upper half
-		if (row < rows0_ && row >= 0) {
-			return DiscreteMatrix(ul_offset_, ur_offset_, 0, 0,
-									1, 0, cols0_, cols1_, 
-									upper_left_ + row * cols0_ * ul_offset_, upper_right_ + row * cols1_ * ur_offset_, NULL, NULL);
-		}
-
-		if (row > rows0_ && row < (rows0_ + rows1_)) {
-			return DiscreteMatrix(ll_offset_, lr_offset_, 0, 0,
-									1, 0, cols0_, cols1_,
-									lower_left_ + (row - rows0_) * ll_offset_, lower_right_ + (row - rows0_) * lr_offset_, NULL, NULL);
-		}
-
-		return DiscreteMatrix();
-	}
-
-	CUDAH bool getSubMatrixFromUpperLeft(int row, int col, DiscreteMatrix *output) {
-		if (row < 0 || col < 0)
-			return false;
-
-		//Get upper left of upper left sub matrix
-		for (int i = 0; i < row && i < rows0_; i++) {
-			for (int j = 0; j < col && j < cols0_; j++) {
-				output->set(i, j, upper_left_[(i * cols0_ + j) * ul_offset_]);
-			}
-		}
-
-		//Get upper right of upper left sub matrix
-		for (int i = row + 1; i < rows0_; i++) {
-			for (int j = 0; j < col && j < cols0_; j++) {
-				output->set(i - 1, j, upper_left_[(i * cols0_ + j) * ul_offset_]);
-			}
-		}
-
-		//Get lower right of upper left sub matrix
-		for (int i = row + 1; i < rows0_; i++) {
-			for (int j = col + 1; j < cols0_; j++) {
-				output->set(i - 1, j - 1, upper_left_[(i * cols0_ + j) * ul_offset_]);
-			}
-		}
-
-		//Get lower left of upper left sub matrix
-		for (int i = 0; i < row && i < rows0_; i++) {
-			for (int j = col + 1; j < cols0_; j++) {
-				output->set(i, j - 1, upper_left_[(i * cols0_ + j) * ul_offset_]);
-			}
-		}
-
-		return true;
-	}
-
-	CUDAH bool getSubMatrixFromLowerLeft(int row, int col, DiscreteMatrix *output) {
-		if (row >= (rows0_ + rows1_) || col < 0)
-			return false;
-
-		for (int i = 0; i < row - rows0_; i++) {
-			for (int j = 0; j < col && j < cols0_; j++) {
-				output->set(i + rows0_, j, lower_left_[(i * cols0_ + j) * ll_offset_]);
-			}
-		}
-
-		for (int i = 0; i < row - rows0_; i++) {
-			for (int j = col + 1; j < cols0_; j++) {
-				output->set(i + rows0_, j - 1, lower_left_[(i * cols0_ + j) * ll_offset_]);
-			}
-		}
-
-		for (int i = ((row >= rows0_) ? row - rows0_ + 1 : 0); i < rows1_; i++) {
-			for (int j = 0; j < col && j < cols0_; j++) {
-				output->set(i + rows0_ - 1, j, lower_left_[(i * cols0_ + j) * ll_offset_]);
-			}
-		}
-
-		for (int i = ((row >= rows0_) ? row - rows0_ + 1 : 0); i < rows1_; i++) {
-			for (int j = col + 1; j < cols0_; j++) {
-				output->set(i + rows0_ - 1, j - 1, lower_left_[(i * cols0_ + j) * ll_offset_]);
-			}
-		}
-
-		return true;
-	}
-
-	CUDAH bool getSubMatrixFromUpperRight(int row, int col, DiscreteMatrix *output) {
-		if (row < 0 || col >= (cols0_ + cols1_))
-			return false;
-
-		for (int i = 0; i < row && i < rows0_; i++) {
-			for (int j = 0; j < col - cols0_; j++) {
-				output->set(i, j + cols0_, upper_right_[(i * cols1_ + j) * ur_offset_]);
-			}
-		}
-
-		for (int i = 0; i < row && i < rows0_; i++) {
-			for (int j = ((col >= cols0_) ? col - cols0_ + 1 : 0); j < cols1_; j++) {
-				output->set(i, j + cols0_ - 1, upper_right_[(i * cols1_ + j) * ur_offset_]);
-			}
-		}
-
-		for (int i = row + 1; i < rows0_; i++) {
-			for (int j = 0; j < col - cols0_; j++) {
-				output->set(i, j + cols0_, upper_right_[(i * cols1_ + j) * ur_offset_]);
-			}
-		}
-
-		for (int i = row + 1; i < rows0_; i++) {
-			for (int j = ((col >= cols0_) ? col - cols0_ + 1 : 0); j < cols1_; j++) {
-				output->set(i, j + cols0_ - 1, upper_right_[(i * cols1_ + j) * ur_offset_]);
-			}
-		}
-
-		return true;
-	}
-
-	CUDAH bool getSubMatrixFromLowerRight(int row, int col, DiscreteMatrix *output) {
-		if (row > (rows0_ + rows1_) || col >(cols0_ + cols1_))
-			return false;
-
-		for (int i = 0; i < row - rows0_; i++) {
-			for (int j = 0; j < col - cols0_; j++) {
-				output->set(i + rows0_, j + cols0_, lower_right_[(i * cols1_ + j) * lr_offset_]);
-			}
-		}
-
-		for (int i = 0; i < row - rows0_; i++) {
-			for (int j = ((col >= cols0_) ? col - cols0_ + 1 : 0); j < cols1_; j++) {
-				output->set(i + rows0_, j + cols0_ - 1, lower_right_[(i * cols1_ + j) * lr_offset_]);
-			}
-		}
-
-		for (int i = ((row >= rows0_) ? row - rows0_ + 1 : 0); i < rows1_; i++) {
-			for (int j = 0; j < col - cols0_; j++) {
-				output->set(i + rows0_, j + cols0_ - 1, lower_right_[(i * cols1_ + j) * lr_offset_]);
-			}
-		}
-
-		for (int i = ((row >= rows0_) ? row - rows0_ + 1 : 0); i < rows1_; i++) {
-			for (int j = ((col >= cols0_) ? col - cols0_ + 1 : 0); j < cols1_; j++) {
-				output->set(i + rows0_ - 1, j + cols0_ - 1, lower_right_[(i * cols1_ + j) * lr_offset_]);
-			}
-		}
-
-		return true;
-	}
-
-	CUDAH bool getSubMatrix(int row, int col, DiscreteMatrix *output) {
-		if (!getSubMatrixFromUpperLeft(row, col, output))
-			return false;
-
-		if (!getSubMatrixFromUpperRight(row, col, output))
-			return false;
-
-		if (!getSubMatrixFromLowerLeft(row, col, output))
-			return false;
-
-		if (!getSubMatrixFromLowerRight(row, col, output))
-			return false;
-
-		return true;
-	}
-
-	CUDAH float det() {
-
-	}
-
-private:
-	float *upper_left_, *upper_right_, *lower_left_, *lower_right_;
-	int ul_offset_, ur_offset_, ll_offset_, lr_offset_;
-	int rows0_, rows1_, cols0_, cols1_;
-};
-
 class Matrix {
 public:
-	Matrix();
+	CUDAH Matrix() {
+		buffer_ = NULL;
+		rows_ = cols_ = offset_ = 0;
+	}
+
 	CUDAH Matrix(int rows, int cols, int offset, float *buffer) {
 		rows_ = rows;
 		cols_ = cols;
@@ -298,16 +33,31 @@ public:
 		}
 	}
 
-	CUDAH float at(int row, int col) const {
+	CUDAH float& operator()(int row, int col) {
+		if (row < 0 || col < 0 || row >= rows_ || col >= cols_)
+			exit(EXIT_FAILURE);
+
 		return buffer_[(row * cols_ + col) * offset_];
 	}
 
-	CUDAH void set(int row, int col, float val) {
+	CUDAH float at(int row, int col) const {
+		if (row < 0 || col < 0 || row >= rows_ || col >= cols_)
+			exit(EXIT_FAILURE);
+
+		return buffer_[(row * cols_ + col) * offset_];
+	}
+
+	CUDAH bool set(int row, int col, float val) {
+		if (row < 0 || col < 0 || row >= rows_ || col >= cols_)
+			return false;
+
 		buffer_[(row * cols_ + col) * offset_] = val;
+
+		return true;
 	}
 
 	CUDAH bool getCol(int col, Matrix *output) {
-		if (col > cols_ || col < 0 || output->rows_ != rows_)
+		if (col >= cols_ || col < 0 || output->rows_ != rows_)
 			return false;
 
 		for (int i = 0; i < rows_; i++) {
@@ -318,6 +68,9 @@ public:
 	}
 
 	CUDAH Matrix getCol(int col) {
+		if (col < 0 || col >= cols_)
+			return Matrix();
+
 		return Matrix(rows_, 1, offset_ * cols_, buffer_ + col * offset_);
 	}
 
@@ -333,6 +86,9 @@ public:
 	}
 
 	CUDAH Matrix getRow(int row) {
+		if (row < 0 || row >= rows_)
+			return Matrix();
+
 		return Matrix(1, cols_, offset_, buffer_ + row * cols_ * offset_);
 	}
 
@@ -374,15 +130,289 @@ public:
 		return true;
 	}
 
-	CUDAH float det() {
+	static CUDAH bool add(const Matrix input0, const Matrix input1, Matrix& output) {
+		if (input0.rows_ != input1.rows_ || input0.rows_ != output.rows_ || input0.cols_ != input1.cols_ || input0.cols_ != output.cols_)
+			return false;
 
+		for (int i = 0; i < input0.rows_; i++) {
+			for (int j = 0; j < input0.cols_; j++) {
+				output.set(i, j, input0.at(i, j) + input1.at(i, j));
+			}
+		}
+
+		return true;
 	}
 
-private:
+	static CUDAH bool add(const Matrix input0, const Matrix input1, Matrix *output) {
+		if (input0.rows_ != input1.rows_ || input0.rows_ != output->rows_ || input0.cols_ != input1.cols_ || input0.cols_ != output->cols_)
+			return false;
+
+		for (int i = 0; i < input0.rows_; i++) {
+			for (int j = 0; j < input0.cols_; j++) {
+				output->set(i, j, input0.at(i, j) + input1.at(i, j));
+			}
+		}
+
+		return true;
+	}
+
+	static CUDAH bool subtract(const Matrix input0, const Matrix input1, Matrix& output) {
+		if (input0.rows_ != input1.rows_ || input0.rows_ != output.rows_ || input0.cols_ != input1.cols_ || input0.cols_ != output.cols_)
+			return false;
+
+		for (int i = 0; i < input0.rows_; i++) {
+			for (int j = 0; j < input0.cols_; j++) {
+				output.set(i, j, input0.at(i, j) - input1.at(i, j));
+			}
+		}
+
+		return true;
+	}
+
+	static CUDAH bool subtract(const Matrix input0, const Matrix input1, Matrix *output) {
+		if (input0.rows_ != input1.rows_ || input0.rows_ != output->rows_ || input0.cols_ != input1.cols_ || input0.cols_ != output->cols_)
+			return false;
+
+		for (int i = 0; i < input0.rows_; i++) {
+			for (int j = 0; j < input0.cols_; j++) {
+				output->set(i, j, input0.at(i, j) - input1.at(i, j));
+			}
+		}
+
+		return true;
+	}
+
+	static CUDAH bool multiply(const Matrix input0, const Matrix input1, Matrix &output) {
+		if (input0.cols_ != input1.rows_ || input0.rows_ != output.rows_ || input1.cols_ != output.cols_)
+			return false;
+
+		for (int i = 0; i < output.rows_; i++) {
+			for (int j = 0; j < output.cols_; j++) {
+				float tmp = 0;
+				for (int k = 0; k < input0.cols_; k++) {
+					tmp += input0.at(i, k) * input1.at(k, j);
+				}
+
+				output.set(i, j, tmp);
+			}
+		}
+
+		return true;
+	}
+
+	static CUDAH bool multiply(const Matrix input0, const Matrix input1, Matrix *output) {
+		if (input0.cols_ != input1.rows_ || input0.rows_ != output->rows_ || input1.cols_ != output->cols_)
+			return false;
+
+		for (int i = 0; i < output->rows_; i++) {
+			for (int j = 0; j < output->cols_; j++) {
+				float tmp = 0;
+				for (int k = 0; k < input0.cols_; k++) {
+					tmp += input0.at(i, k) * input1.at(k, j);
+				}
+
+				output->set(i, j, tmp);
+			}
+		}
+
+		return true;
+	}
+
+	CUDAH bool scalarMultiply(float val, Matrix &output) {
+		if (rows_ != output.rows_ || cols_ != output.cols_)
+			return false;
+
+		for (int i = 0; i < rows_; i++) {
+			for (int j = 0; j < cols_; j++) {
+				output(i, j) = buffer_[(i * cols_ + j) * offset_] * val;
+			}
+		}
+
+		return true;
+	}
+
+	CUDAH bool scalarDivide(float val, Matrix &output) {
+		if (rows_ != output.rows_ || cols_ != output.cols_ || val == 0)
+			return false;
+
+		for (int i = 0; i < rows_; i++) {
+			for (int j = 0; j < cols_; j++) {
+				output(i, j) = buffer_[(i * cols_ + j) * offset_] / val;
+			}
+		}
+
+		return true;
+	}
+
+	CUDAH bool operator*=(float val) {
+		for (int i = 0; i < rows_; i++) {
+			for (int j = 0; j < cols_; j++) {
+				buffer_[(i * cols_ + j) * offset_] *= val;
+			}
+		}
+
+		return true;
+	}
+
+	CUDAH bool operator/=(float val) {
+		if (val == 0)
+			return false;
+
+		for (int i = 0; i < rows_; i++) {
+			for (int j = 0; j < cols_; j++) {
+				buffer_[(i * cols_ + j) * offset_] /= val;
+			}
+		}
+
+		return true;
+	}
+
+	CUDAH bool transpose(Matrix &output) {
+		if (rows_ != output.cols_ || cols_ != output.rows_)
+			return false;
+
+		for (int i = 0; i < rows_; i++) {
+			for (int j = 0; j < cols_; j++) {
+				output(j, i) = buffer_[(i * cols_ + j) * offset_];
+			}
+		}
+
+		return true;
+	}
+protected:
 	float *buffer_;
 	int rows_, cols_, offset_;
-}
+};
 
+class SquareMatrix : protected Matrix {
+public:
+	CUDAH SquareMatrix() {
+		rows_ = cols_ = 0;
+		offset_ = 0;
+		buffer_ = NULL;
+	}
+
+	CUDAH SquareMatrix(int size, int offset, float *buffer) {
+		rows_ = cols_ = size;
+		offset_ = offset;
+		buffer_ = buffer;
+	}
+
+	CUDAH float det(float *temp_buffer) {
+		if (rows_ != cols_)
+			exit(EXIT_FAILURE);
+
+		if (rows_ == 1)
+			return buffer_[0];
+
+		if (rows_ == 2)
+			return (buffer_[0] * buffer_[3 * offset_] - buffer_[offset_] * buffer_[2 * offset_]);
+
+		if (rows_ == 3)
+			return (buffer_[0] * buffer_[4 * offset_] * buffer_[8 * offset_] + buffer_[offset_] * buffer_[5 * offset_] * buffer_[6 * offset_]
+				+ buffer_[2 * offset_] * buffer_[3 * offset_] * buffer_[7 * offset_] - buffer_[2 * offset_] * buffer_[4 * offset_] * buffer_[6 * offset_]
+				- buffer_[offset_] * buffer_[3 * offset_] * buffer_[8 * offset_] - buffer_[0] * buffer_[5 * offset_] * buffer_[7 * offset_]);
+
+		float retval = 0;
+
+		for (int i = 0, float sign = 1; i < rows_; i++, sign *= (-1)) {
+			SquareMatrix sub_matrix(rows_ - 1, offset_, temp_buffer);
+
+			getSubMatrix(0, i, &sub_matrix);
+
+			retval += sign * sub_matrix.det(temp_buffer + (rows_ - 1) * (rows_ - 1) * offset_);
+		}
+
+		return retval;
+	}
+
+	CUDAH bool inverse(float *temp_buffer, Matrix *output) {
+		if (rows_ != cols_)
+			return false;
+
+		float det_val = det(temp_buffer);
+
+		if (det_val)
+			return false;
+
+		if (rows_ == 1) {
+			output->set(0, 0, 1 / det_val);
+
+			return true;
+		}
+
+		for (int i = 0, float sign = 1; i < rows_; i++) {
+			for (int j = 0; j < cols_; j++, sign *= (-1)) {
+				SquareMatrix sub_matrix(rows_ - 1, offset_, temp_buffer);
+
+				getSubMatrix(i, j, &sub_matrix);
+
+				output->set(i, j, sign * 1 / det_val * sub_matrix.det(temp_buffer + (rows_ - 1) * (cols_ - 1) * offset_));
+			}
+		}
+
+		return true;
+	}
+};
+
+class IdentityMatrix : protected SquareMatrix {
+public:
+	CUDAH IdentityMatrix() {
+		rows_ = cols_ = 0;
+		offset_ = 0;
+		buffer_ = NULL;
+	}
+
+	CUDAH IdentityMatrix(int size, int offset, float *buffer) {
+		rows_ = size;
+		cols_ = size;
+		offset_ = offset;
+		buffer_ = buffer;
+
+		for (int i = 0; i < rows_; i++) {
+			for (int j = 0; j < cols_; j++) {
+				buffer[(i * cols_ + j) * offset_] = (i == j) ? 1 : 0;
+			}
+		}
+	}
+};
+
+class Vector : protected Matrix {
+public:
+	CUDAH Vector() {
+		rows_ = cols_ = offset_ = 0;
+		buffer_ = NULL;
+	}
+
+	CUDAH Vector(int size, int offset, float *buffer) {
+		rows_ = 1;
+		cols_ = size;
+		offset_ = offset;
+		buffer_ = buffer;
+	}
+
+	CUDAH float& operator()(int index) {
+		if (index < 0 || index >= cols_)
+			exit(EXIT_FAILURE);
+
+		return buffer_[index * offset_];
+	}
+
+	CUDAH float at(int index) const {
+		if (index < 0 || index >= cols_)
+			exit(EXIT_FAILURE);
+
+		return buffer_[index * offset_];
+	}
+
+	CUDAH float operator*(const Vector input) const {
+		float retval = 0;
+
+		for (int i = 0; i < cols_; i++) {
+			retval += buffer_[i * offset_] * input.at(i);
+		}
+	}
+};
 
 
 }
